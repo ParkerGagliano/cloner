d49a3f85381c18828b8dbcbe01347a92e7d46302
Parker <pjg4086@uncw.edu>
2023-11-05 15:10:05 -0500

    clean up

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-use std::intrinsics::r#try;
-use std::os::unix::process;
-
 use clap::Parser;
 use dialoguer::{MultiSelect, Select};
 use reqwest;
@@ -13,10 +10,31 @@ struct Args {
     #[arg(short, long)]
     gh_token: String,
 }
+const ORG_PROMPT: &'static str = "Select the organization you want to clone repos from... ARROW KEYS: Next/Prev Page SPACE: Select, ENTER: Confirm";
+const REPO_PROMPT: &'static str = "Select the repos you want to clone... ARROW KEYS: Next/Prev Page SPACE: Select, ENTER: Confirm";
+fn retrieve_selection(multi: bool, items: &Vec<String>, prompt: &str) -> Vec<usize> {
+    if multi {
+        let selection: Vec<usize> = MultiSelect::new()
+            .with_prompt(prompt)
+            .items(&items)
+            .interact()
+            .unwrap();
+        selection
+    } else {
+        let selection: usize = Select::new()
+            .with_prompt("Select the organization you want to clone repos from... ARROW KEYS: Next/Prev Page SPACE: Select, ENTER: Confirm ")
+            .items(&items)
+            .interact()
+            .unwrap();
+        let mut vec = Vec::new();
+        vec.push(selection);
+        vec
+    }
+}
 
 async fn fetch(url: &str, token: &str) -> Result<Value, Error> {
-    let client = reqwest::Client::new();
-    let res = client
+    let client: reqwest::Client = reqwest::Client::new();
+    let res: reqwest::Response = client
         .get(url)
         .header("Authorization", format!("Bearer {}", token))
         .header("User-Agent", "reqwest")
@@ -25,17 +43,6 @@ async fn fetch(url: &str, token: &str) -> Result<Value, Error> {
 
     let body = res.bytes().await?; // Get response body as bytes
     let json_response: Value = serde_json::from_slice(&body).unwrap();
-    Ok(json_response)
-}
-
-async fn fetch_orgs(token: &str) -> Result<Value, Error> {
-    let json_response = fetch("https://api.github.com/user/orgs", &token).await?;
-    Ok(json_response)
-}
-
-async fn fetch_repos(repo_link: &str, token: &str) -> Result<Value, Error> {
-    let repo_link = repo_link.trim_matches('"');
-    let json_response = fetch(&repo_link, &token).await?;
     Ok(json_response)
 }
 
@@ -53,14 +60,26 @@ fn clone_repo(repo_url: &str, gh_token: &str) {
         .arg(command)
         .output()
         .expect("failed to execute process");
-
     println!("{}", String::from_utf8_lossy(&output.stdout));
 }
 
+async fn fetch_orgs(token: &str) -> Result<Vec<Value>, Error> {
+    let json_response = fetch("https://api.github.com/user/orgs", &token).await?;
+    let array: Vec<Value> = json_response.as_array().unwrap().to_owned();
+    Ok(array)
+}
+
+async fn get_repo_links(repo_link: &str, qparams: &str, token: &str) -> Result<Vec<Value>, Error> {
+    let repo_link = repo_link.trim_matches('"').to_owned() + qparams;
+    let json_response = fetch(&repo_link, &token).await?;
+    let array: Vec<Value> = json_response.as_array().unwrap().to_owned();
+    Ok(array)
+}
+
 fn clone_selected_repos(selected_repos: Vec<Value>, gh_token: &str) {
-    let mut repo_urls = Vec::new();
-    selected_repos.iter().for_each(|repo| {
-        let repo_url = repo["clone_url"].to_string();
+    let mut repo_urls: Vec<String> = Vec::new();
+    selected_repos.iter().for_each(|repo: &Value| {
+        let repo_url: String = repo["clone_url"].to_string();
         repo_urls.push(repo_url);
     });
 
@@ -68,49 +87,42 @@ fn clone_selected_repos(selected_repos: Vec<Value>, gh_token: &str) {
         clone_repo(url, gh_token);
     });
 }
-
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let args = Args::parse();
-    let body;
+    let args: Args = Args::parse();
+    let body: Vec<Value>;
     if let Ok(orgs) = fetch_orgs(&args.gh_token).await {
         body = orgs;
     } else {
-        println!("Invalid token");
+        println!("Unable to fetch organizations");
         std::process::exit(1);
     }
+    let selection: Vec<usize> = retrieve_selection(
+        false,
+        &body
+            .iter()
+            .map(|org| org["login"].to_string())
+            .collect::<Vec<String>>(),
+        ORG_PROMPT,
+    );
 
-    let body: &Vec<Value> = body.as_array().unwrap();
-
-    let selection: usize = Select::new()
-        .with_prompt("Select the organization you want to clone repos from... ARROW KEYS: Next/Prev Page SPACE: Select, ENTER: Confirm ")
-        .items(&body.iter().map(|org| org["login"].to_string()).collect::<Vec<String>>())
-        .interact()
-        .unwrap();
-
-    let current_repo: String = body[selection]["repos_url"].to_string();
-    let repos: &Vec<Value>;
-    if let Ok(repos_ok) = fetch_repos(&(current_repo + "?per_page=100"), &args.gh_token).await {
-        if let Some(valid_repos) = repos_ok.as_array() {
-            // as_array doesnt return error or not, it returns Some or None
-            repos = valid_repos;
-        } else {
-            println!("No Repos Found");
-            std::process::exit(1);
-        }
+    let current_repo: String = body[selection[0]]["repos_url"].to_string();
+    let repos: Vec<Value>;
+    println!("{}", current_repo);
+    if let Ok(repos_ok) = get_repo_links(&current_repo, "?per_page=100", &args.gh_token).await {
+        repos = repos_ok //.to_owned();
     } else {
         println!("Invalid token");
         std::process::exit(1);
     };
-
-    let selection = MultiSelect::new()
-        .with_prompt("Select the repos you want to clone... ARROW KEYS: Next/Prev Page SPACE: Select, ENTER: Confirm ")
-        .items(&repos
+    let selection: Vec<usize> = retrieve_selection(
+        true,
+        &repos
             .iter()
             .map(|repo| repo["name"].to_string())
-            .collect::<Vec<String>>())
-        .interact()
-        .unwrap();
+            .collect::<Vec<String>>(),
+        REPO_PROMPT,
+    );
 
     let selected_raw_body = selection
         .iter()
